8f19a849822142debd000f474824a4e5cf03b504
mnagaev <mnagaev@roox.ru>
2020-05-10 17:22:39 +0300

    Updates

---
+++ precache-manifest.ba4673b5c6f9951934316c629052f0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c486dca51191f9a74d18a23cc34e9ad1",
+    "url": "/cra-products-crud/index.html"
+  },
+  {
+    "revision": "301722e3eeb85e87f78b",
+    "url": "/cra-products-crud/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7d9fe2f4fe7e4a292f70",
+    "url": "/cra-products-crud/static/css/main.4f4555ee.chunk.css"
+  },
+  {
+    "revision": "301722e3eeb85e87f78b",
+    "url": "/cra-products-crud/static/js/2.dd1c88f0.chunk.js"
+  },
+  {
+    "revision": "7d9fe2f4fe7e4a292f70",
+    "url": "/cra-products-crud/static/js/main.c0cc4ebb.chunk.js"
+  },
+  {
+    "revision": "8d6a8308d2a1e63f86c4",
+    "url": "/cra-products-crud/static/js/runtime~main.5aeee459.js"
+  }
+]);
